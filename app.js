2a366923ad25034052583d1b8a418fd77ddc3879
Nick Garcia <nick@nicks-mbp.myfiosgateway.com>
2022-05-29 09:47:05 -0400

    add letters

---
+++ app.js
@@ -0,0 +1,11 @@
+function makeElement(type, attributes, text) {
+    const newEl = document.createElement(type);
+    for (let attr of attributes) {
+      newEl.setAttribute(attr, atributes[attr])
+    }
+  }
+  
+  const h1 = makeElement('hi', {
+    class: 'title',
+    id: 'blhhh'
+  })
